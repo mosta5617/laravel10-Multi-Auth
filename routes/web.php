ca1450ef7ed74fa7cf7b04c7ef6733485e2d036d
Mostafijur-Rahman <mosta5617@gmail.com>
2023-04-11 00:12:21 +0600

    Admin Agent User Role Added

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Admin\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AgentController;
 use App\Http\Controllers\ProfileController;
 use GuzzleHttp\Promise\Create;
 use Illuminate\Support\Facades\Route;
@@ -33,4 +35,12 @@ Route::middleware('auth')->group(function () {
 require __DIR__.'/auth.php';
 
 
+Route::middleware(['auth', 'role:admin'] )->group(function(){
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    
+});
 
+Route::middleware(['auth', 'role:agent'] )->group(function(){
+    Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
+    
+});
