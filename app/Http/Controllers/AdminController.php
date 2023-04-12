ecab5c40e02ab1220f099ec0b5c342c86268c174
Mostafijur-Rahman <mosta5617@gmail.com>
2023-04-12 08:52:24 +0600

    admin logout route added

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function AdminDashboard(){
         return view('admin.index');
 
+    }
+
+    public function AdminLogout(Request $request){
+        Auth::guard('web')->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -37,6 +37,7 @@ require __DIR__.'/auth.php';
 
 Route::middleware(['auth', 'role:admin'] )->group(function(){
     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
     
 });
 
