a4045bbc90cf30ccfa30b559d491a19d3bac0674
Mostafijur-Rahman <mosta5617@gmail.com>
2023-04-16 23:41:07 +0600

    profile image updated

--- resources/views/admin/admin_profile_view.blade.php
+++ resources/views/admin/admin_profile_view.blade.php
@@ -10,26 +10,25 @@
           <div class="card-body">
             <div class="d-flex align-items-center justify-content-between mb-2">
                 <div class="d-flex align-items-center">
-                    <img class="wd-100 rounded-circle" src="https://via.placeholder.com/100x100" alt="">													
+                    <img class="wd-100 rounded-circle" src="{{ (!empty($profile_data->photo))? url('upload/admin-images/'.$profile_data->photo) : url('upload/no-image.jpg') }}" alt="profile">													
                 </div>
-                <h6 class="card-title mb-0">About</h6>
-            </div>
-            <p>Hi! I'm Amiah the Senior UI Designer at NobleUI. We hope you enjoy the design and quality of Social.</p>
-            <div class="mt-3">
-              <label class="tx-11 fw-bolder mb-0 text-uppercase">Joined:</label>
-              <p class="text-muted">November 15, 2015</p>
+                <h6 class="card-title mb-0">{{ $profile_data->username }}</h6>
             </div>
             <div class="mt-3">
-              <label class="tx-11 fw-bolder mb-0 text-uppercase">Lives:</label>
-              <p class="text-muted">New York, USA</p>
+              <label class="tx-11 fw-bolder mb-0 text-uppercase">Name: </label>
+              <p class="text-muted">{{ $profile_data->name }}</p>
             </div>
-            <div class="mt-3">
+            <div class="mt-3"> 
               <label class="tx-11 fw-bolder mb-0 text-uppercase">Email:</label>
-              <p class="text-muted">me@nobleui.com</p>
+              <p class="text-muted">{{ $profile_data->email }}</p>
             </div>
             <div class="mt-3">
-              <label class="tx-11 fw-bolder mb-0 text-uppercase">Website:</label>
-              <p class="text-muted">www.nobleui.com</p>
+              <label class="tx-11 fw-bolder mb-0 text-uppercase">Phone:</label>
+              <p class="text-muted">{{ $profile_data->phone }}</p>
+            </div>
+            <div class="mt-3">
+              <label class="tx-11 fw-bolder mb-0 text-uppercase">Address:</label>
+              <p class="text-muted">{{ $profile_data->address }}</p>
             </div>
             <div class="mt-3 d-flex social-links">
               <a href="javascript:;" class="btn btn-icon border btn-xs me-2">
@@ -59,11 +58,11 @@
                                       <form class="forms-sample">
                                           <div class="mb-3">
                                               <label for="exampleInputUsername1" class="form-label">Username</label>
-                                              <input type="text" class="form-control" id="exampleInputUsername1" autocomplete="off" placeholder="Username">
+                                              <input type="text" class="form-control" id="exampleInputUsername1" autocomplete="off" placeholder="{{ $profile_data->username }}">
                                           </div>
                                           <div class="mb-3">
                                               <label for="exampleInputEmail1" class="form-label">Email address</label>
-                                              <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                                              <input type="email" class="form-control" id="exampleInputEmail1" placeholder="{{ $profile_data->email }}">
                                           </div>
                                           <div class="mb-3">
                                               <label for="exampleInputPassword1" class="form-label">Password</label>
