e03c3786993f349d40863aca4f9d5fd505874ed8
Mostafijur-Rahman <mosta5617@gmail.com>
2023-05-07 23:57:07 +0600

    .

--- routes/web.php
+++ routes/web.php
@@ -53,7 +53,6 @@ Route::middleware(['auth', 'role:admin'] )->group(function(){
 
 });
 
-
 Route::middleware(['auth', 'role:agent'] )->group(function(){
     Route::get('/agent/dashboard', [AgentController::class, 'AgentDashboard'])->name('agent.dashboard');
     
